08a37ca4c8040ae74a80fee966a68f8fdd2b9ffe
Serdar KÖKTAŞ <serdarkkts@gmail.com>
2020-03-31 01:00:38 +0300

    PID

--- main.go
+++ main.go
@@ -19,22 +19,19 @@ func main() {
 }
 
 func run() {
-
-	fmt.Printf("Running %v \n", os.Args[2:])
 	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS,
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID,
 	}
 
 	must(cmd.Run())
 }
 
 func child() {
-
-	fmt.Printf("Running %v \n", os.Args[2:])
+	fmt.Printf("Running %v as PID %d\n", os.Args[2:], os.Getpid())
 	cmd := exec.Command(os.Args[2], os.Args[3:]...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
