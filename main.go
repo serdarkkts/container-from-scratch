68ecb627a50eb0e6baaf29b6dc6dcca9ddc21015
Serdar KÖKTAŞ <serdarkkts@gmail.com>
2020-03-31 00:35:28 +0300

    child process

--- main.go
+++ main.go
@@ -11,6 +11,8 @@ func main() {
 	switch os.Args[1] {
 	case "run":
 		run()
+	case "child":
+		child()
 	default:
 		panic("help")
 	}
@@ -19,13 +21,26 @@ func main() {
 func run() {
 
 	fmt.Printf("Running %v \n", os.Args[2:])
-	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.SysProcAttr = &syscall.SysProcAttr{
 		Cloneflags: syscall.CLONE_NEWUTS,
 	}
+
+	must(cmd.Run())
+}
+
+func child() {
+
+	fmt.Printf("Running %v \n", os.Args[2:])
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	must(syscall.Sethostname([]byte("container")))
 
 	must(cmd.Run())
 }
